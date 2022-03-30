aca2ff96bb9b0c61784b1d8491255c0a4887f102
Jose Pablo <30580990+josepablomc@users.noreply.github.com>
2022-03-29 20:40:08 -0600

    NC-51 + NC-60 / Agregando CRUD API para Gastos y Proveedores.

---
+++ API/Controllers/Gastos.cs
@@ -0,0 +1,143 @@
+﻿using AutoMapper;
+using DAL.DO.Objects;
+using DAL.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
+using models = API.DataModels;
+
+namespace API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GastosController : Controller
+    {
+        private readonly NDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GastosController(NDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        // GET: api/Gastos
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<models.Gastos>>> GetGastos()
+        {
+
+            var res = new BE.Gastos(_context).GetAll();
+            List<models.Gastos> mapaAux = _mapper.Map<IEnumerable<data.Gastos>, IEnumerable<models.Gastos>>(res).ToList();
+            return mapaAux;
+        }
+
+
+        // GET: api/Gastos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<models.Gastos>> GetGastos(int id)
+        {
+            var Gastos = new BE.Gastos(_context).GetOneById(id);
+
+            if (Gastos == null)
+            {
+                return NotFound();
+            }
+            models.Gastos mapaAux = _mapper.Map<data.Gastos, models.Gastos>(Gastos);
+            return mapaAux;
+
+        }
+
+        // PUT: api/Gastos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGastos(int id, models.Gastos Gastos)
+        {
+            if (id != Gastos.IdGasto) //OJO que sea el mismo nombre de la columna ID de la DB --Karen 19/03/2022
+            {
+                return BadRequest();
+            }
+
+
+            try
+            {
+                data.Gastos mapaAux = _mapper.Map<models.Gastos, data.Gastos>(Gastos);
+                //return mapaAux;
+                new BE.Gastos(_context).Update(mapaAux);
+
+            }
+            catch (Exception ee)
+            {
+                if (!Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //private bool CategoriesExists(int id)
+        //    {
+        //        return ( new BE.BE.Gastos(_context).GetOneById(id) != null);
+        //    }
+
+        // POST: api/Gastos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<models.Gastos>> PostGastos(models.Gastos Gastos)
+        {
+            try
+            {
+                data.Gastos mapaAux = _mapper.Map<models.Gastos, data.Gastos>(Gastos);
+                new BE.Gastos(_context).Insert(mapaAux);
+            }
+            catch (Exception)
+            {
+
+                BadRequest();
+            }
+
+
+            return CreatedAtAction("GetGastos", new { id = Gastos.IdGasto }, Gastos); //OJO que sea el mismo nombre de la columna ID de la DB --Karen 19/03/2022
+        }
+
+        // DELETE: api/Gastos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<models.Gastos>> DeleteGastos(int id)
+        {
+            var Gastos = new BE.Gastos(_context).GetOneById(id);
+            if (Gastos == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                new BE.Gastos(_context).Delete(Gastos);
+            }
+            catch (Exception)
+            {
+
+                BadRequest();
+            }
+
+            models.Gastos mapaAux = _mapper.Map<data.Gastos, models.Gastos>(Gastos);
+            return mapaAux;
+        }
+
+        private bool Exists(int id)
+        {
+            return (new BE.Gastos(_context).GetOneById(id) != null);
+        }
+    }
+}
---
+++ API/Controllers/Proveedores.cs
@@ -0,0 +1,143 @@
+﻿using AutoMapper;
+using DAL.DO.Objects;
+using DAL.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
+using models = API.DataModels;
+
+namespace API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedoresController : Controller
+    {
+        private readonly NDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProveedoresController(NDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        // GET: api/Proveedores
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<models.Proveedores>>> GetProveedores()
+        {
+
+            var res = new BE.Proveedores(_context).GetAll();
+            List<models.Proveedores> mapaAux = _mapper.Map<IEnumerable<data.Proveedores>, IEnumerable<models.Proveedores>>(res).ToList();
+            return mapaAux;
+        }
+
+
+        // GET: api/Proveedores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<models.Proveedores>> GetProveedores(int id)
+        {
+            var Proveedores = new BE.Proveedores(_context).GetOneById(id);
+
+            if (Proveedores == null)
+            {
+                return NotFound();
+            }
+            models.Proveedores mapaAux = _mapper.Map<data.Proveedores, models.Proveedores>(Proveedores);
+            return mapaAux;
+
+        }
+
+        // PUT: api/Proveedores/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProveedores(int id, models.Proveedores Proveedores)
+        {
+            if (id != Proveedores.IdProveedor) //OJO que sea el mismo nombre de la columna ID de la DB --Karen 19/03/2022
+            {
+                return BadRequest();
+            }
+
+
+            try
+            {
+                data.Proveedores mapaAux = _mapper.Map<models.Proveedores, data.Proveedores>(Proveedores);
+                //return mapaAux;
+                new BE.Proveedores(_context).Update(mapaAux);
+
+            }
+            catch (Exception ee)
+            {
+                if (!Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //private bool CategoriesExists(int id)
+        //    {
+        //        return ( new BE.BE.Proveedores(_context).GetOneById(id) != null);
+        //    }
+
+        // POST: api/Proveedores
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<models.Proveedores>> PostProveedores(models.Proveedores Proveedores)
+        {
+            try
+            {
+                data.Proveedores mapaAux = _mapper.Map<models.Proveedores, data.Proveedores>(Proveedores);
+                new BE.Proveedores(_context).Insert(mapaAux);
+            }
+            catch (Exception)
+            {
+
+                BadRequest();
+            }
+
+
+            return CreatedAtAction("GetProveedores", new { id = Proveedores.IdProveedor }, Proveedores); //OJO que sea el mismo nombre de la columna ID de la DB --Karen 19/03/2022
+        }
+
+        // DELETE: api/Proveedores/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<models.Proveedores>> DeleteProveedores(int id)
+        {
+            var Proveedores = new BE.Proveedores(_context).GetOneById(id);
+            if (Proveedores == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                new BE.Proveedores(_context).Delete(Proveedores);
+            }
+            catch (Exception)
+            {
+
+                BadRequest();
+            }
+
+            models.Proveedores mapaAux = _mapper.Map<data.Proveedores, models.Proveedores>(Proveedores);
+            return mapaAux;
+        }
+
+        private bool Exists(int id)
+        {
+            return (new BE.Proveedores(_context).GetOneById(id) != null);
+        }
+    }
+}
