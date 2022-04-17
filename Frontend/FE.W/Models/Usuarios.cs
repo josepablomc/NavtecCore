8fab7570ca8f42e42326a499c27d704fc482c0bc
Jose Pablo <30580990+josepablomc@users.noreply.github.com>
2022-04-17 00:19:24 -0600

    NC-26 / NC-52 / NC-79 / NC-61
    Adding Frontend Modules for
    Cotizaciones
    Gastos
    Servicios
    Proveedores

---
+++ Frontend/FE.W/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace FE.W.Models
+{
+    public partial class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public int IdRol { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CorreoUsuario { get; set; }
+        public string ClaveUsuario { get; set; }
+
+        public virtual Roles IdRolNavigation { get; set; }
+    }
+}
